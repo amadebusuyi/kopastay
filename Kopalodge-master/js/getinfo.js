57f4b5ffa183ef9b12a417c7d6ab5544f7c9208e
amadebusuyi <52937844+amadebusuyi@users.noreply.github.com>
2020-02-13 17:23:54 +0100

    Add files via upload

--- Kopalodge-master/js/getinfo.js
+++ Kopalodge-master/js/getinfo.js
@@ -1,16 +1,17 @@
 // Your web app's Firebase configuration
   var firebaseConfig = {
-    apiKey: "AIzaSyBfFBh2KDfWWyPaFLNusOtjUCcPQWapaw0",
-    authDomain: "kato-me.firebaseapp.com",
-    databaseURL: "https://kato-me.firebaseio.com",
-    projectId: "kato-me",
-    storageBucket: "kato-me.appspot.com",
-    messagingSenderId: "466286957993",
-    appId: "1:466286957993:web:cb6a430c57a29cce33a0b8",
-    measurementId: "G-J0TSYQWEQW"
+    apiKey: "AIzaSyAPZdWdRzDHwyinXLFBSUSWfWDwvkZzF_M",
+    authDomain: "kopa-stay.firebaseapp.com",
+    databaseURL: "https://kopa-stay.firebaseio.com",
+    projectId: "kopa-stay",
+    storageBucket: "kopa-stay.appspot.com",
+    messagingSenderId: "658458955460",
+    appId: "1:658458955460:web:6bacdb772c105b5c2f8391",
+    measurementId: "G-83YNDVYFCS"
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
 
 
 function isEmpty($element){
@@ -21,24 +22,46 @@ function isEmpty($element){
         return true;
     }
 }
-let dbLink = firebase.database();
+//let dbLink = firebase.database();
 
 $(document).ready(function(){
   let added = new Date();
     added = added.toISOString().slice(0, 16);
-    var uid = Math.random().toString(36).substring(2, 5) + Math.random().toString(36).substring(2, 5);
-
+ //   var uid = Math.random().toString(36).substring(2, 5) + Math.random().toString(36).substring(2, 5);
  $("#submit").click(function(){
+  if($("#name").val() === ""){
+                $("#name").focus();
+                $(".error-name").show();
+                return false;
+            }
+            else if($("#email").val() == "" || $("#email").val().indexOf("@") < 3){
+                $("#email").focus();
+                $(".error-email").show();
+                return false;
+            }
+
+            else if ($("#phone").val() === "" || $("#phone").val().length < 11){
+                $("#phone").focus();
+                $(".error-phone").show();
+                return false;
+            }
+
   let statePos = Number($("#state").val()) - 1;
   let state = nigeria[statePos].state.name;
   let min = $(".slider-range-value-1").html();
   let max = $(".slider-range-value-2").html();
   let budget = min + " to " + max;
-let wMsg = "Hello, I am "+$("#name").val()+" (Email: "+$("#email").val()+") and I am looking for a "+
-  $("#apartment").val() + " within the price range of "+budget+" in "+ $("#lga").val() + ", "+ state + ". "+
-  "Description: "+ $("#desc").val() + ".";
+  if($("#desc").val() !== ""){
+   let msgDesc = "Description: "+ $("#desc").val() + ".";
+  }
+  else msgDesc = "";
+let wMsg = "Hello, I am "+$("#name").val()+" and I am looking for a "+
+  $("#apartment").val() + " within the price range of "+budget+" in "+ $("#lga").val() + ", "+ state + "."+
+  msgDesc;
 
- dbLink.ref('kopastay/userInfo/' + uid).set({
+  var uid = firebase.database().ref().child('userInfo').push().key;
+
+ firebase.database().ref('userInfo/' + uid).set({
   name : $("#name").val(),
   email : $("#email").val(),
   phone : $("#phone").val(),
@@ -51,14 +74,14 @@ let wMsg = "Hello, I am "+$("#name").val()+" (Email: "+$("#email").val()+") and 
   },
   function(error){
       if(error){
-        $(".form-h1").html("Thanks for using our service "+ $("#name").val());
+        $(".form-h1").html("Thanks for using our service <b>"+ $("#name").val()+ "</b>");
         let msg =  '<p class="alert alert-danger"><h3>Your information '+
         'could not be submitted due to technical issues.</h3></p>'+
           '<hr>'+
           '<p class="whatsapp">'+
           'You can get across to us on WhatsApp right away.'+
           '<br>'+ 
-          '<a href="https://api.whatsapp.com/send?phone=08065348422&text='+wMsg+'"'+
+          '<a href="https://api.whatsapp.com/send?phone=2349038371598&text='+wMsg+'"'+
           ' class="btn btn-success btn-lg">Message on'+
           ' WhatsApp</a>'+
           '</p>';
@@ -69,14 +92,14 @@ let wMsg = "Hello, I am "+$("#name").val()+" (Email: "+$("#email").val()+") and 
         $("#feedback").show();
       }
       else{ 
-        $(".form-h1").html("Thanks for using our service "+ $("#name").val());
+        $(".form-h1").html("Thanks for using our service <b>"+ $("#name").val()+"</b>");
         let msg =  '<p><h3>Your information has been submitted and you will get'+
         ' a response from us within <b>24 hours</b>. </h3></p>'+
                        '<hr>'+
                         '<p class="whatsapp">'+
                         'You can get across to us on WhatsApp right away.'+
                         '<br>'+ 
-          '<a href="https://api.whatsapp.com/send?phone=08065348422&text='+wMsg+'"'+
+          '<a href="https://api.whatsapp.com/send?phone=2349038371598&text='+wMsg+'"'+
           ' class="btn btn-success btn-lg">Message on'+
           ' WhatsApp</a>'+
                         '</p>';  
